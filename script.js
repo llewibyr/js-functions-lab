99eba1211cd933181dce8ecf2a1e3807d9e386cd
llewibyr <byronnlew@gmail.com>
2024-07-04 14:05:49 -0400

    Finished 1 through 7, will do level up soon

--- script.js
+++ script.js
@@ -32,16 +32,16 @@ Example: isAdult(21) should return 'Adult'.
 
 Complete the exercise in the space below:
 */
-function isAdult(age) {
+// function isAdult(age) {
 // console.log(age, 16 >= 18)
-     age = 21
+//     age = 21
      
-     if(age >= 18) {
-       return 'Adult'
-    }   else {
-       return 'Minor'
-    }
- }
+//     if(age >= 18) {
+//       return 'Adult'
+//    }   else {
+//       return 'Minor'
+//    }
+// }
 
 // let age = 21
 
@@ -50,8 +50,123 @@ function isAdult(age) {
 // } else () { 
 //     console.log("Minor")
 // }
+// console.log('Exercise 2 Result:', isAdult(21));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+ function isCharAVowel(vowel){
+ if(vowel === "a" || vowel === "e" || vowel === "i" || vowel === "o" || vowel === "u") {
+   return "True"
+ } else {
+   return "False"
+ }
+}
 
 
 
+// console.log('Exercise 3 Result:', isCharAVowel("r"));
 
-console.log('Exercise 2 Result:', isAdult(21));
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+ function generateEmail(name, domain) {
+ console.log()
+    return `${name}@${domain}`;
+ }
+
+
+// console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+ function greetUser(name, timeOfDay) {
+    
+     if (timeOfDay = "morning"){
+     return (`Goodmorning, ${name}!`)
+      } else if (timeOfDay = "afternoon") {
+      return(`Good afternoon, ${name}!`)
+     } else if (timeOfDay = "evening") {
+        return(`Good evening, ${name}!`)
+     }
+ }
+ 
+
+
+// console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+function maxOfThree(a, b, c) { 
+  if (a >= b && a >= c) {
+    return a;
+  } else if (b >= a && b >= c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+let result = maxOfThree(5, 10, 8)
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+function calculateTip(billAmount, tipPercentage) {
+let tipAmount = (billAmount * tipPercentage) / 100;
+return tipAmount;
+}
+
+let billAmount = 50;
+let tipPercentage = 20;
+
+let tip = calculateTip(billAmount, tipPercentage);
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+
